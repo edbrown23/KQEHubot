c4907d509bdf87f2dc5457f6422e65ac4d60e962
Eric Brown <eric.d.brown23@gmail.com>
2013-08-12 23:41:21 -0400

    Fixing some environment vars

--- hubot_runner.sh
+++ hubot_runner.sh
@@ -6,6 +6,9 @@ export HUBOT_IRC_ROOMS="#KQEProject"
 export HUBOT_IRC_NICK="hubot"
 export HUBOT_IRC_UNFLOOD="true"
 
+export HUBOT_DARK_SKY_DEFAULT_LOCATION="Boston"
+export HUBOT_DARK_SKY_API_KEY="9fbb5026b456df477a0105700fbb51dc"
+
 while [ true ]; do
     ./bin/hubot -a irc --name hubot
 done
